e61d8305f9779edcaf080b4fba94ad7514bc3b34
Abhijith P <abhijith@riafy.me>
2022-04-29 22:25:31 +0530

    Made Poster component responsive

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, Image, StyleSheet, useWindowDimensions} from 'react-native';
 
 import {Images} from '../assets/images';
 import {Typography} from '../styles';
 
-const {width: deviceWidth} = Dimensions.get('window');
-const width = (deviceWidth - 4 * 20) / 3;
-const height = width * 1.49;
+const Poster = ({name, posterUrl}) => {
+  /**
+   * this hook is used to make Poster response if device width change,
+   * like during device rotation.
+   */
+  const {width: deviceWidth} = useWindowDimensions();
 
-const Poster = ({name, posterUrl}) => (
-  <View style={styles.posterContainer}>
-    <Image
-      source={Images?.[posterUrl] ?? Images.placeholder}
-      style={styles.imageStyle}
-      resizeMode="cover"
-    />
-    <Text style={[Typography.h3, styles.name]} numberOfLines={2}>
-      {name}
-    </Text>
-  </View>
-);
+  const width = (deviceWidth - 4 * 20) / 3;
+  const height = width * 1.49;
+
+  return (
+    <View style={styles.posterContainer}>
+      <Image
+        source={Images?.[posterUrl] ?? Images.placeholder}
+        style={{height, width}}
+        resizeMode="cover"
+      />
+      <Text style={[Typography.h3, styles.name, {width}]} numberOfLines={2}>
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   posterContainer: {margin: 10},
-  name: {width, minHeight: 40, marginTop: 3},
-  imageStyle: {height, width},
+  name: {minHeight: 40, marginTop: 3},
 });
 
 export default Poster;
